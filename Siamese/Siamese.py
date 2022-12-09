07ac9f5f7bbd1e3daad42de75115e1b1147d8984
Junbo <junbo012799@gmail.com>
2022-12-09 17:21:41 -0500

    updated evalutaion function

--- Siamese/Siamese.py
+++ Siamese/Siamese.py
@@ -1,14 +1,14 @@
 from torch.nn import Module, Conv2d, Linear, ReLU, MaxPool2d
 
 class CNN(Module):
-    def __init__(self):
+    def __init__(self, embSize=5):
         super(CNN, self).__init__()
         self.conv1 = Conv2d(3, 6, 5)
         self.pool = MaxPool2d(2, 2)
         self.conv2 = Conv2d(6, 16, 5)
         self.fc1 = Linear(16 * 5 * 5, 120)
         self.fc2 = Linear(120, 84)
-        self.fc3 = Linear(84, 50)
+        self.fc3 = Linear(84, embSize)
         self.relu = ReLU()
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
